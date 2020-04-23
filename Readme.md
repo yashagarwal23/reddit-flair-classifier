c95c0711a5d5b9cf4d9fe9f1a0d58b149a7d5639
Yash Agarwal <yash23.agarwal@gmail.com>
2020-04-23 15:01:37 +0530

    added Readme and webserver

---
+++ Readme.md
@@ -0,0 +1,40 @@
+# Reddit Flair detector
+
+MIDAS Assignment
+
+## Data Acquisition
+
+Used the [pushshift](https://pushshift.io/) API to scrape posts and submissions from Reddit. This API had a much larger dataset available than the rate limited official Reddit API as well as PRAW. The posts are taken from 4 different times over the last 5 years to have a good mix of flairs. As pushshift returns at max 1000 posts in a single request, posts time stamps are used to avoid repeated posts. Raw pushshift data dump can be found [here](https://drive.google.com/file/d/1uRjj-v0AXgGQpKlM3swt5FzQrugcUD12/view?usp=sharing)
+
+The submission body text is cleaned by removing punctuation marks, removing extra white spaces and then converting the whole text to lower case. Also unidentified tokens like Hindi Font words are also removed.
+
+All flairs with less than 2000 submissions have been removed for a better view and ambiguous flairs like ```Non-political``` , ```Scheduled``` , ```Repost``` etc were removed. Same flairs with different names were merged together.
+
+Selected flairs with flair count :- 
+
+![](selected_flairs_with_count.png)
+
+Further to avoid inter-class imbalance due to the large differences in training instances of various classes, i have limited the amount of submissions upto 20,000 for each class.
+
+Final Flair Count :- 
+
+![](final_flair_count.png)
+
+
+
+## Models
+
+To classify a flair I have used both the title and body of the post. If the body of the flair is not present (it is an image, is a URL or written Hindi), then only the title is used. First i trained a language model on the data. Then used this trained language model as the encoder for the classifier and fine-tuned them together.
+
+Link to the model :-  https://drive.google.com/file/d/1-1ONGZa231gH9jDMJY3doNnrc_POHrEa/view?usp=sharing
+
+### Result
+
+Achieved about ```78% ``` accuracy considering both the title and body (if present) of the reddit flair.
+
+
+
+### Prediction
+
+![prediction](prediction.png)
+
---
+++ webserver/app.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, flash, request, send_file, jsonify
+#  import json
+from wtforms import Form, TextField, validators
+from fastai.basic_train import load_learner
+import praw
+import re
+import os
+import requests
+import pickle
+import nltk
+
+app = Flask(__name__)
+DEBUG = True
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+
+reddit = praw.Reddit(client_id = "CE_CHBOiktT0mw",
+					client_secret = "1_hli9su-rwvR2dv2_0jxuJNLqA",
+					user_agent = "reddit_scraper",
+					username = "yashagarwal23",
+					password = "Hisar*123")
+
+classifier = load_learner(".", 'reddit_flair_classifier')
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", text)
+    text = re.sub(r"\d+", "", text).strip()
+    return text
+
+def predict(url):
+    submission = reddit.submission(url = url)
+    text = re.sub("\s\s+" , " ", submission.title + ". " + submission.selftext)
+    text = clean_text(text)
+    return str(classifier.predict(text)[0])
+
+
+class ReusableForm(Form):
+    name = TextField('Name:', validators=[validators.required()])
+
+    @app.route("/", methods=['GET', 'POST'])
+    def hello():
+        form = ReusableForm(request.form)
+        print(form.errors)
+        if request.method == 'POST':
+            url=request.form['name']
+            prediction = predict(url)
+            if form.validate():
+            # Save the comment here.
+                flash("Predicted Flair : " + prediction)
+            else:
+                flash('Error: URL required. ')
+        return render_template('index.html', form=form)
+
+@app.route('/automated_testing', methods=['POST', 'GET'])
+def automated_testing():
+    if request.method == 'GET':
+        return "automated testing. post a file with a reddit post url in each line for flair prediction"
+    file = request.files['file']
+    urls = file.readlines()
+    prediction = {url.decode("utf-8"):predict(url.decode("utf-8")) for url in urls}
+    print(prediction)
+    return jsonify(prediction)
+    #  json.dump(prediction, result_file)
+    #  return send_file("results.json", mimetype='text/csv', as_attachment=True)
+
+if __name__ == "__main__":
+    app.run(debug=DEBUG)
